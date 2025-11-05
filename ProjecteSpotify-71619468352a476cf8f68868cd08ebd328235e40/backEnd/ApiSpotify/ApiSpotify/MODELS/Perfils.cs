9a1fd2c6d506faed5cbcbf74f74abcdde74c7b45
mpicotti <mpicotti472@boscdelacoma.cat>
2025-11-05 17:24:33 +0100

    Creacio del Model Perfils

---
+++ ProjecteSpotify-71619468352a476cf8f68868cd08ebd328235e40/backEnd/ApiSpotify/ApiSpotify/MODELS/Perfils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiSpotify.MODELS
+{
+    internal class Perfils
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid IdUsuari { get; set; }
+        public string Nom { get; set; }
+        public string Descripcio { get; set; }
+        public string Estat { get; set; }
+
+    }
+}
